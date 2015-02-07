eb59681e2c91c2f03cac755a3a2e08e812695b86
Usuario cube <ucube@localhost.localdomain>
2015-02-06 18:21:09 -0600

    Agacha el personaje

--- js/saltoKen.js
+++ js/saltoKen.js
@@ -1,6 +1,7 @@
- var objKen = function( arriba ){
+ var objKen = function( arriba, abajo ){
 
    this.saltoArriba = arriba;
+   this.agachar = abajo;
 
    this.salto = function(){
       var salta = this.saltoArriba; 
@@ -10,19 +11,36 @@
             {
               var objDivKen =  $( ".divKen" );
               objDivKen.addClass( "ken" );
-              setTimeout( function(){ QuitarSalto( objDivKen );} , 1000 );
+              setTimeout( function(){ QuitarEventos( objDivKen );} , 1000 );
             }
       } ); 
    }
 
-   var QuitarSalto = function( objDivKen ){
-     objDivKen.removeClass( "ken" );
+   this.abajo = function(){
+     var agachar = this.agachar;
+     $( "body" ).keydown( function( ev ){
+           if ( ev.which == agachar )
+              {
+                var objDivKen =  $( ".divKen" );
+                objDivKen.addClass( "kenAbajo" );
+                setTimeout( function(){ QuitarEventos( objDivKen );} , 400 );
+              }
+     } );  
+   }
+
+   var QuitarEventos = function( objDivKen ){
+    if ( objDivKen.hasClass( "ken" ) )
+         objDivKen.removeClass( "ken" );
+    
+    if ( objDivKen.hasClass( "kenAbajo" ) )
+         objDivKen.removeClass( "kenAbajo" );
    } 
  
  }
 
 $( document ).ready( function(){
- var objPersonajeKen = new objKen( 38 );
+ var objPersonajeKen = new objKen( 38, 40 );
  objPersonajeKen.salto();
+ objPersonajeKen.abajo();
 
 } ); 
